bad8e684d10ffe6f18abec88703dc0403786207c
emilymcumminsMIST <90233217+emilymcumminsMIST@users.noreply.github.com>
2021-10-15 15:32:24 -0700

    lab7 updates

---
+++ IST 541/lab7/js/hotspot.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+
+if ($(this).attr("id") == "light bulb") {
+         $("#feedback").css("color","green");
+         $("#feedback").html("We are learning about Thermometers and Weather Icons!");
+
+$("#feedback").css("backgroundColor","yellow");
+$('#myModal').modal("show");
+});
+}); //end main jQuery function
